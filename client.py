a3ecc7e0db3fd3ff3085d986ef142be1d14457c6
Bence Topa <bencetopa06@gmail.com>
2025-12-22 10:22:06 +0100

    Made it so that the 2 way communication works so you can send a message whenever you want

--- client.py
+++ client.py
@@ -1,10 +1,27 @@
 import socket
+import threading
+import time
+
+user_input= None
+def input_thread():
+    global user_input
+    while True:
+        user_input=input()
+threading.Thread(target=input_thread, daemon=True).start()
+
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 9999))
-
+client.setblocking(False)
 
 while True:
-    message=input("Type your message: ")
-    client.send(message.encode())
-    print(client.recv(1024).decode())
+    if user_input is not None:
+        client.send(user_input.encode())
+        user_input=None
+    try:
+        data=client.recv(1024)
+        if data:
+            print(data.decode())
+    except BlockingIOError:
+        pass
+    time.sleep(0.01)
     
--- server.py
+++ server.py
@@ -1,14 +1,30 @@
 import socket
+import threading
+import time
+
+user_input= None
+def input_thread():
+    global user_input
+    while True:
+        user_input=input()
+threading.Thread(target=input_thread, daemon=True).start()
 
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1', 9999))
-
 server.listen(1)
+
 
 while True:
     client, addr = server.accept()
+    client.setblocking(False)
     while True:
-        print(client.recv(1024).decode())
-        message=input("Type a message: ")
-        client.send(message.encode())
-    #client.send('server test'.encode())
+        try:
+            data=client.recv(1024)
+            if data:
+                print(data.decode())
+        except BlockingIOError:
+            pass
+        if user_input is not None:
+            client.send(user_input.encode())
+            user_input=None
+        time.sleep(0.01)
