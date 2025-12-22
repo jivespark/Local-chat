80bcd9f414f8086b14d048bc9bd064aebffa77b6
Bence Topa <bencetopa06@gmail.com>
2025-12-22 09:18:44 +0100

    I made it so the 2 codes can communicate with eachother through localhost

---
+++ client.py
@@ -0,0 +1,6 @@
+import socket
+client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 9999))
+
+client.send('client test'.encode())
+print(client.recv(1024).decode())
